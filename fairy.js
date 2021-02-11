eab9aff3a838060568c59c102218f6bd38baed5c
matrikaGhosh <76779639+matrikaGhosh@users.noreply.github.com>
2021-02-11 16:30:18 +0530

    Add files via upload

---
+++ fairy.js
@@ -0,0 +1,26 @@
+class fairy{
+constructor(x,y){
+this.image= loadImage("images/fairy.png")
+
+}
+display(){
+ this.body.posotion.x = mouseX;
+ this.body.posotion.y = mouseY;
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
